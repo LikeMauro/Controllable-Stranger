02dc7f428f3a10d307ce34639cd0e5cfcc582ee9
LikeMauro <104032732+LikeMauro@users.noreply.github.com>
2022-06-14 18:21:36 +0200

    Added the feature to start the departure early

--- ControllableStranger/ControllableStranger.cs
+++ ControllableStranger/ControllableStranger.cs
@@ -21,6 +21,36 @@ namespace ControllableStranger
 
         private void Update()
         {
+            if (Keyboard.current.numpad1Key.wasPressedThisFrame)
+            {
+                Locator.GetRingWorldController().BeginDeploySails();
+                Locator.GetRingWorldController().BeginLightFlicker();
+                Locator.GetRingWorldController().BeginDepartSolarSystem();
+            }
+
+            if (Locator.GetRingWorldController()._sailsDeploying == true && Keyboard.current.numpad0Key.wasPressedThisFrame)
+            {
+                if (Locator.GetRingWorldController()._playerInsideRingWorld)
+                {
+                    Locator.GetRingWorldController()._solarSailOneShot.PlayOneShot(global::AudioType.SolarSail_RW_End, 1f);
+                    Locator.GetRingWorldController()._solarSailLooping.FadeOut(0.2f, OWAudioSource.FadeOutCompleteAction.STOP, 0f);
+                    Locator.GetRingWorldController()._damOneShotAudio_Far.PlayOneShot(global::AudioType.StationShudder_DW, 1f);
+                    if (Locator.GetPlayerController().GetGroundBody() == Locator.GetRingWorldController()._ringWorldBody)
+                    {
+                        RumbleManager.PlayStationShudder(1f);
+                    }
+                }
+                if (Locator.GetDreamWorldAudioController() != null)
+                {
+                    Locator.GetDreamWorldAudioController().OnSolarSailStop();
+                }
+                Locator.GetRingWorldController()._solarSailClosedCollider.SetActivation(false);
+                Locator.GetRingWorldController()._solarSailOpenCollider.SetActivation(true);
+                Locator.GetRingWorldController()._solarSailClosedProxy.SetActive(false);
+                Locator.GetRingWorldController()._solarSailOpenProxy.SetActive(true);
+                Locator.GetRingWorldController()._sailsDeployed = true;
+            }
+            
             if (Keyboard.current.numpad2Key.wasPressedThisFrame)
             {
                 Locator.GetRingWorldController().BreakDam();
